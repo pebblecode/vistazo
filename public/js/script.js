7c2cf6e86a277bb0f59fad47290b370e0526d609
Tak Tran <contact@tutaktran.com>
2011-11-22 09:58:27 +0000

    Add adhoc close button.

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,20 @@
 $(function () {
+  $("#new-project-form").hide(); // Hide by default
+  
   $(".new-project").click(function () {
     $(this).css({opacity: "1"});
-    $("#d-test").animate({opacity: "1"});
+    $("#new-project-form").show();
   });
+  
+  $("#new-project-form .close").click(function() {
+    $("#new-project-form").hide();
+    return false;
+  });
+  
 });
 
 $(function () {
   $(".new-project").hover(function () {
-  
     $(this).animate({opacity: "1"});
     
   }, function () {
