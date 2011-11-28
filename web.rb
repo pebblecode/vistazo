94bb18cb773562c827eb290271275c59d8b5a451
Tak Tran <contact@tutaktran.com>
2011-11-28 17:50:32 +0000

    Add pebbleit and pebble code .net team to seed data. For #35.

--- web.rb
+++ web.rb
@@ -404,7 +404,35 @@ post '/reset' do
   ])
   
   pebble_code_dot_net = Account.create(:name => "pebble{code} .net team", :url_slug => "pebble_code_dot_net")
+  pebble_code_dot_net.update_attributes(:projects => [
+    Project.create(:name => "Contrarius"),
+    Project.create(:name => "Bingo")
+  ])
+  pebble_code_dot_net.update_attributes(:team_members => [
+    TeamMember.create(:name => "Toby H"),
+    TeamMember.create(:name => "Alex B"),
+    TeamMember.create(:name => "Greg J"),
+    TeamMember.create(:name => "Matt W"),
+    TeamMember.create(:name => "Daniel B")
+  ])
+  
   pebble_it = Account.create(:name => "pebble.it", :url_slug => "pebble_code_dot_net")
+  pebble_it.update_attributes(:projects => [
+    Project.create(:name => "Frukt"),
+    Project.create(:name => "Kane")
+  ])
+  pebble_it.update_attributes(:team_members => [
+    TeamMember.create(:name => "Toby H"),
+    TeamMember.create(:name => "Seb N"),
+    TeamMember.create(:name => "Paul E"),
+    TeamMember.create(:name => "David O"),
+    TeamMember.create(:name => "Graham G"),
+    TeamMember.create(:name => "Simon T"),
+    TeamMember.create(:name => "Michael P"),
+    TeamMember.create(:name => "James F"),
+    TeamMember.create(:name => "Toby TAG G"),
+    TeamMember.create(:name => "Gayle S")
+  ])
 
   flash[:success] = "Successfully cleared out the database and added seed data. Enjoy!"
   redirect '/'
