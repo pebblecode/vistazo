eac00db430de3e45834bead420c53dd5ac33d57f
Tak Tran <contact@tutaktran.com>
2011-11-28 18:40:09 +0000

    Routing for account pages. For #35.

--- web.rb
+++ web.rb
@@ -254,46 +254,66 @@ get '/' do
   erb :homepage
 end
 
+get '/:account' do
+  protected!
+  
+  @account = Account.find_by_url_slug(params[:account])
+  
+  if @account.present?
+    redirect "/#{params[:account]}/#{Time.now.year}/week/#{Time.now.strftime("%U")}"
+  else
+    flash.next[:warning] = "Invalid account."
+    redirect '/'
+  end
+end
 
 get '/:account/:year/week/:week_num' do
   protected!
   
-  year = params[:year].to_i
-  week_num = params[:week_num].to_i
+  @account = Account.find_by_url_slug(params[:account])
   
-  if ((1..NUM_WEEKS_IN_A_YEAR).include? week_num) and (year > START_YEAR)
-    # Weeks start from 1
-    prev_week_num = ((week_num - 1) <= 0) ? NUM_WEEKS_IN_A_YEAR : week_num - 1
-    prev_week_year = ((week_num - 1) <= 0) ? year - 1 : year
-    @prev_week_url = (prev_week_year > START_YEAR) ? "/#{prev_week_year}/week/#{prev_week_num}" : nil
+  if @account.present?
+    year = params[:year].to_i
+    week_num = params[:week_num].to_i
+  
+    if ((1..NUM_WEEKS_IN_A_YEAR).include? week_num) and (year > START_YEAR)
+      # Weeks start from 1
+      prev_week_num = ((week_num - 1) <= 0) ? NUM_WEEKS_IN_A_YEAR : week_num - 1
+      prev_week_year = ((week_num - 1) <= 0) ? year - 1 : year
+      @prev_week_url = (prev_week_year > START_YEAR) ? "/#{prev_week_year}/week/#{prev_week_num}" : nil
     
-    next_week_num = ((week_num + 1) > NUM_WEEKS_IN_A_YEAR) ? 1 : week_num + 1
-    next_week_year = ((week_num + 1) > NUM_WEEKS_IN_A_YEAR) ? year + 1 : year
-    @next_week_url = "/#{next_week_year}/week/#{next_week_num}"
+      next_week_num = ((week_num + 1) > NUM_WEEKS_IN_A_YEAR) ? 1 : week_num + 1
+      next_week_year = ((week_num + 1) > NUM_WEEKS_IN_A_YEAR) ? year + 1 : year
+      @next_week_url = "/#{next_week_year}/week/#{next_week_num}"
 
-    @monday_date = Date.commercial(year, week_num, MONDAY)
-    @tuesday_date = Date.commercial(year, week_num, TUESDAY)
-    @wednesday_date = Date.commercial(year, week_num, WEDNESDAY)
-    @thursday_date = Date.commercial(year, week_num, THURSDAY)
-    @friday_date = Date.commercial(year, week_num, FRIDAY)
+      @monday_date = Date.commercial(year, week_num, MONDAY)
+      @tuesday_date = Date.commercial(year, week_num, TUESDAY)
+      @wednesday_date = Date.commercial(year, week_num, WEDNESDAY)
+      @thursday_date = Date.commercial(year, week_num, THURSDAY)
+      @friday_date = Date.commercial(year, week_num, FRIDAY)
   
-    @projects = Project.sort(:name)
-    @team_members = TeamMember.sort(:name)
+      @projects = Project.sort(:name)
+      @team_members = TeamMember.sort(:name)
 
-    # Assume it's the right week of dates
-    @team_member_projects_on_day = {}
-    for tm in @team_members do
-      @team_member_projects_on_day[tm] = {}
+      # Assume it's the right week of dates
+      @team_member_projects_on_day = {}
+      for tm in @team_members do
+        @team_member_projects_on_day[tm] = {}
 
-      (MONDAY..FRIDAY).each do |work_day|
-        @team_member_projects_on_day[tm][work_day] = tm.team_member_projects.select { |proj| 
-          (proj.date.wday == work_day) and (proj.date >= @monday_date) and (proj.date <= @friday_date)
-        }
+        (MONDAY..FRIDAY).each do |work_day|
+          @team_member_projects_on_day[tm][work_day] = tm.team_member_projects.select { |proj| 
+            (proj.date.wday == work_day) and (proj.date >= @monday_date) and (proj.date <= @friday_date)
+          }
+        end
       end
-    end
 
-    erb :week
+      erb :week
+    else
+      flash.next[:warning] = "Invalid week and year."
+      redirect "/#{params[:account]}"
+    end
   else
+    flash.next[:warning] = "Invalid account."
     redirect '/'
   end
 end
