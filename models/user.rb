f59f23ad8ed635c39e2348cfd243d79ba4a43bce
Vince Martinez <eykosioux@gmail.com>
2011-12-01 17:37:03 +0000

    User model

---
+++ models/user.rb
@@ -0,0 +1,14 @@
+class User
+  include MongoMapper::Document
+
+  key :name, String, :required => true
+  key :uid, String, :required => true
+  key :email, String, :required => true
+  timestamps!
+
+  # Relationships
+  belongs_to :account
+
+
+  # Instance methods
+end
