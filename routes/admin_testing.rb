2fb97a561e2df19b8615892951149d2fdd603849
Tak Tran <contact@tutaktran.com>
2011-12-02 16:58:23 +0000

    Remove account/project/team_member creation in admin testing. Just delete all the data in reset.

--- routes/admin_testing.rb
+++ routes/admin_testing.rb
@@ -9,53 +9,6 @@ class VistazoApp < Sinatra::Application
     Account.delete_all()
     User.delete_all()
   
-    # Seed data
-    pebble_code_web_dev = Account.create(:name => "pebble{code} web-dev team")
-    pebble_code_web_dev.update_attributes(:projects => [
-      Project.create(:name => "ideapi"),
-      Project.create(:name => "Space"),
-      Project.create(:name => "LDN taxi"),
-      Project.create(:name => "Vistazo")
-    ])
-    pebble_code_web_dev.update_attributes(:team_members => [
-      TeamMember.create(:name => "Toby H"),
-      TeamMember.create(:name => "George O"),
-      TeamMember.create(:name => "Mark D"),
-      TeamMember.create(:name => "Tak T"),
-      TeamMember.create(:name => "Vince M"),
-    ])
-  
-    pebble_code_dot_net = Account.create(:name => "pebble{code} .net team")
-    pebble_code_dot_net.update_attributes(:projects => [
-      Project.create(:name => "Contrarius"),
-      Project.create(:name => "Bingo")
-    ])
-    pebble_code_dot_net.update_attributes(:team_members => [
-      TeamMember.create(:name => "Toby H"),
-      TeamMember.create(:name => "Alex B"),
-      TeamMember.create(:name => "Greg J"),
-      TeamMember.create(:name => "Matt W"),
-      TeamMember.create(:name => "Daniel B")
-    ])
-  
-    pebble_it = Account.create(:name => "pebble.it")
-    pebble_it.update_attributes(:projects => [
-      Project.create(:name => "Frukt"),
-      Project.create(:name => "Kane")
-    ])
-    pebble_it.update_attributes(:team_members => [
-      TeamMember.create(:name => "Toby H"),
-      TeamMember.create(:name => "Seb N"),
-      TeamMember.create(:name => "Paul E"),
-      TeamMember.create(:name => "David O"),
-      TeamMember.create(:name => "Graham G"),
-      TeamMember.create(:name => "Simon T"),
-      TeamMember.create(:name => "Michael P"),
-      TeamMember.create(:name => "James F"),
-      TeamMember.create(:name => "Toby TAG G"),
-      TeamMember.create(:name => "Gayle S")
-    ])
-
     flash[:success] = "Successfully cleared out the database and added seed data. Enjoy!"
     redirect '/'
   end
