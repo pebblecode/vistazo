bc457fcabef4a33a613fb3c8f7e3dac5bd8155d3
Tak Tran <contact@tutaktran.com>
2011-12-06 11:52:14 +0000

    Save pending users, after registration step. For #44.

--- routes/authentication.rb
+++ routes/authentication.rb
@@ -3,26 +3,42 @@ class VistazoApp < Sinatra::Application
     hash = request.env['omniauth.auth'].to_hash
     @user = User.find_by_uid(hash["uid"])
     unless @user.present?
-      @user = User.create(
-        :uid   => hash["uid"],
-        :name  => hash["info"]["name"],
-        :email => hash["info"]["email"]
-      )
-      unless @user.valid?
-        flash[:warning] = "Could not retrieve user."
-        @user = nil
-        redirect '/'
+      @user = User.find_by_email(hash["info"]["email"])
+
+      if @user.is_pending? # Occurs after registration
+        @user.uid = hash["uid"]
+        @user.name = hash["info"]["name"]
+        
+        @user.save
+
+        if @user.valid?
+          flash[:success] = "Welcome to Vistazo! You've successfully registered."
+        else
+          flash[:warning] = "Could not register user."
+          puts @user.errors
+          @user = nil
+          redirect '/'
+        end
+      else
+        @user = User.create(
+          :uid   => hash["uid"],
+          :name  => hash["info"]["name"],
+          :email => hash["info"]["email"]
+        )
+        if @user.valid?
+          @account = create_account
+
+          # Add the user as the first team member
+          @account.team_members << TeamMember.create(:name => @user.name)
+
+          flash[:success] = "Welcome to Vistazo! We're ready for you to add projects for yourself."
+        else
+          flash[:warning] = "Could not retrieve user."
+          puts @user.errors
+          @user = nil
+          redirect '/'
+        end
       end
-    end
-    
-    @account = @user.account
-    unless @account
-      @account = create_account
-      
-      # Add the user as the first team member
-      @account.team_members << TeamMember.create(:name => @user.name)
-      
-      flash[:success] = "Welcome to Vistazo! We're ready for you to add projects for yourself."
     end
     
     session['uid'] = @user.uid
