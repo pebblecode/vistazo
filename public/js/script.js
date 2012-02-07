3be5766754516de1ac5958230a5dd13e5c3de262
Tak Tran <contact@tutaktran.com>
2012-02-07 11:28:29 -0800

    Templates work!!

--- public/js/script.js
+++ public/js/script.js
@@ -82,10 +82,16 @@ $(function () {
         rowClass: "row1",
         tmProjects: {
           1: {
-            id: "222",
+            id: "111",
             date: "2012-02-06",
             project_name: "Some project",
             css_class: "Some project"
+          },
+          2: {
+            id: "222",
+            date: "2012-02-07",
+            project_name: "Some project 2",
+            css_class: "Some project 2"
           }
         }
       };
